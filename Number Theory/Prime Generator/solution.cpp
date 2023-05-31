684f52dd0ea026d35596c168954b90e75d22be00
alyalsayed <alyalsayed56@yahoo.com>
2023-05-31 03:24:44 +0300

    upload prime generator problem

---
+++ Number Theory/Prime Generator/solution.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+bool is_prime(int n) {
+    if (n < 2) return false;
+    for (int i = 2; i <= sqrt(n); i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int m, n;
+        cin >> m >> n;
+        for (int i = m; i <= n; i++) {
+            if (is_prime(i)) cout << i << endl;
+        }
+        cout << endl;
+    }
+    return 0;
+}
