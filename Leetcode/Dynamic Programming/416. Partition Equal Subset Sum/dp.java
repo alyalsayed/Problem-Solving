de32324da572ede1f42de15284cf3e30f0fabb55
alyalsayed <alyalsayed56@yahoo.com>
2025-02-07 01:03:57 +0200

    feat : add 416. Partition Equal Subset Sum

---
+++ Leetcode/Dynamic Programming/416. Partition Equal Subset Sum/dp.java
@@ -0,0 +1,24 @@
+class Solution {
+     public boolean canPartition(int[] nums) {
+        int totalSum = 0;
+        for (int num : nums) {
+            totalSum += num;
+        }
+
+        if (totalSum % 2 != 0) {
+            return false;
+        }
+
+        int target = totalSum / 2;
+        boolean[] dp = new boolean[target + 1];
+        dp[0] = true;  
+
+        for (int num : nums) {
+            for (int j = target; j >= num; j--) {
+                dp[j] = dp[j] || dp[j - num];
+            }
+        }
+
+        return dp[target];
+    }
+}
